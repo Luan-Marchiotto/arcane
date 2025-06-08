66ed1d70158f29ce52c2843dbcc9201ff9751b21
Luan Marchiotto Silva <luanmarchiotto@gmail.com>
2025-06-08 09:33:47 -0300

    Chat and task

--- oraculo/models.py
+++ oraculo/models.py
@@ -7,3 +7,14 @@ class Treinamentos(models.Model):
     
     def __str__(self):
         return self.site
+    
+class DataTreinamento(models.Model):
+    metadata = models.JSONField(null=True, blank=True)
+    texto = models.TextField(null=True, blank=True)
+
+class Pergunta(models.Model):
+    data_treinamento = models.ManyToManyField(DataTreinamento)
+    pergunta = models.TextField()
+
+    def __str__(self):
+        return self.pergunta
